7145cf26915a18d886660398b9d7c9c43791d4f8
Halina Karastseliova <halina.karastseliova@gmail.com>
2024-01-30 01:12:12 +0200

    feat: add solution for the File system - read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,25 @@
+import { promises as fsPromises } from 'fs';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const fileName = 'fileToRead.txt';
+    const folderPath = './files';
+    const filePath = path.join(folderPath, fileName);
+
+    try {
+        // asynchronously read a content of the file
+        const fileContent = await fsPromises.readFile(filePath, 'utf8');
+        console.log('File content:');
+        console.log(fileContent);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            // throw an error if the file doesn't exist
+            throw new Error('FS operation failed');
+        } else {
+            // if any other error occurs show it in console
+            console.error(error.message);
+        }
+    }
 };
 
 await read();
