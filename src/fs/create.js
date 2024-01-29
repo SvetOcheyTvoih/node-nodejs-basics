e5e51fab5bb499472f039a0bfec6ea83aeaad5c8
Halina Karastseliova <halina.karastseliova@gmail.com>
2024-01-29 21:27:21 +0200

    feat: add solutiion for the File system - create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
-};
+    // async function createFreshFile() {
+        const fileName = 'fresh.txt';
+        const folderPath = './files';
+        const filePath = path.join(folderPath, fileName);
+        const fileContent = 'I am fresh and young';
+
+        try {
+            await fs.access(filePath);
+            throw new Error('FS operation failed: File already exists.');
+        } catch (error) {
+            if (error.code === 'ENOENT') {
+                await fs.writeFile(filePath, fileContent);
+                console.log('File created successfully:', filePath);
+            } else {
+                console.error(error.message);
+            }
+        }
+    }
 
 await create();
