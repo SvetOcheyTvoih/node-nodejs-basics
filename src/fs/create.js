6bc942e48858b470b82521670ede4712a8fb28c8
Halina Karastseliova <halina.karastseliova@gmail.com>
2024-01-30 01:21:08 +0200

    fix: update error message for the File system - create.js task

--- src/fs/create.js
+++ src/fs/create.js
@@ -2,23 +2,22 @@ import fs from 'fs/promises';
 import path from 'path';
 
 const create = async () => {
-    // async function createFreshFile() {
-        const fileName = 'fresh.txt';
-        const folderPath = './files';
-        const filePath = path.join(folderPath, fileName);
-        const fileContent = 'I am fresh and young';
+    const fileName = 'fresh.txt';
+    const folderPath = './files';
+    const filePath = path.join(folderPath, fileName);
+    const fileContent = 'I am fresh and young';
 
-        try {
-            await fs.access(filePath);
-            throw new Error('FS operation failed: File already exists.');
-        } catch (error) {
-            if (error.code === 'ENOENT') {
-                await fs.writeFile(filePath, fileContent);
-                console.log('File created successfully:', filePath);
-            } else {
-                console.error(error.message);
-            }
+    try {
+        await fs.access(filePath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await fs.writeFile(filePath, fileContent);
+            console.log('File created successfully:', filePath);
+        } else {
+            console.error(error.message);
         }
     }
+};
 
 await create();
