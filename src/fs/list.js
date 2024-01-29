d028f5a99764b0ef5839ed0f324babb903a13406
Halina Karastseliova <halina.karastseliova@gmail.com>
2024-01-30 01:52:55 +0200

    feat: add solution for the File system - list.js task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,29 @@
+import { promises as fsPromises } from 'fs';
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = './files';
+
+    try {
+        // check if 'files' folder exists
+        await fsPromises.access(folderPath);
+
+        // read files in the folder
+        const fileNames = await fsPromises.readdir(folderPath);
+
+        // print filenames to the console
+        console.log(`The 'files' folder contains the following files:`);
+        fileNames.forEach((fileName) => {
+            console.log(fileName);
+        });
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            // throw an error if the folder doesn't exist
+            throw new Error('FS operation failed');
+        } else {
+            // if any other error occurs show it in console
+            console.error(error.message);
+        }
+    }
 };
 
 await list();
